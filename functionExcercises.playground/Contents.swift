079c87c67281fa9578168cfceb70d9926e2d5748
Solomon Chan <solomc1@uci.edu>
2016-07-29 17:48:51 -0700

    adding files

---
+++ functionExcercises.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func add (var1: Double, var2: Double) -> Double {
+    let result: Double = var1 + var2
+    return result
+}
+
+func subtract (var1: Double, var2: Double) -> Double {
+    let result: Double = var1 - var2
+    return result
+}
+
+func multiply (var1: Double, var2: Double) -> Double {
+    let result: Double = var1 * var2
+    return result
+}
+
+func divide (var1: Double, var2: Double) -> Double {
+    let result: Double = var1/var2
+    return result
+}
+
+add (5.4, var2: 6.3)
+subtract (10.6, var2: 3.2)
+multiply (3.9, var2: 6.8)
+divide (4.8, var2: 3.1)
